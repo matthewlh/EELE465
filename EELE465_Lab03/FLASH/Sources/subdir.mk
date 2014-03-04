4d35f37978fe85b83a9c746e7abe90327ddeba10
matthewlh <matthewlhandley@gmail.com>
2014-03-04 13:39:54 -0700

    Moved all the bus stuff to bus.asm

--- EELE465_Lab03/FLASH/Sources/subdir.mk
+++ EELE465_Lab03/FLASH/Sources/subdir.mk
@@ -6,39 +6,54 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 ASM_SRCS += \
+../Sources/bus.asm \
 ../Sources/main.asm \
 
 ASM_SRCS_QUOTED += \
+"../Sources/bus.asm" \
 "../Sources/main.asm" \
 
 OBJS += \
+./Sources/bus_asm.obj \
 ./Sources/main_asm.obj \
 
 ASM_DEPS += \
+./Sources/bus_asm.d \
 ./Sources/main_asm.d \
 
 OBJS_QUOTED += \
+"./Sources/bus_asm.obj" \
 "./Sources/main_asm.obj" \
 
 ASM_DEPS_QUOTED += \
+"./Sources/bus_asm.d" \
 "./Sources/main_asm.d" \
 
 OBJS_OS_FORMAT += \
+./Sources/bus_asm.obj \
 ./Sources/main_asm.obj \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Sources/main_asm.obj: ../Sources/main.asm
+Sources/bus_asm.obj: ../Sources/bus.asm
 	@echo 'Building file: $<'
 	@echo 'Executing target #1 $<'
 	@echo 'Invoking: HCS08 Assembler'
-	"$(HC08ToolsEnv)/ahc08" -ArgFile"Sources/main.args" -Objn"Sources/main_asm.obj" "$<" -Lm="$(@:%.obj=%.d)" -LmCfg=xilmou
+	"$(HC08ToolsEnv)/ahc08" -ArgFile"Sources/bus.args" -Objn"Sources/bus_asm.obj" "$<" -Lm="$(@:%.obj=%.d)" -LmCfg=xilmou
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Sources/%.d: ../Sources/%.asm
 	@echo 'Regenerating dependency file: $@'
 	
+	@echo ' '
+
+Sources/main_asm.obj: ../Sources/main.asm
+	@echo 'Building file: $<'
+	@echo 'Executing target #2 $<'
+	@echo 'Invoking: HCS08 Assembler'
+	"$(HC08ToolsEnv)/ahc08" -ArgFile"Sources/main.args" -Objn"Sources/main_asm.obj" "$<" -Lm="$(@:%.obj=%.d)" -LmCfg=xilmou
+	@echo 'Finished building: $<'
 	@echo ' '
 
 
--- EELE465_Lab03/Sources/main.asm
+++ EELE465_Lab03/Sources/main.asm
@@ -9,6 +9,7 @@
 
 mDataBus		EQU	$F0		; Mask for the data bus pins on PortB
 mAddrBus		EQU	$0F		; Mask for the address bus pins on PortB
+
 ADCS1_CH2		EQU %00000010	;ADCS1 configured for CH2
 ADCS1_CH26		EQU %00011010	;ADCS1 configured for CH26 (internal temp. sensor) 
 
@@ -18,12 +19,15 @@ ADCS1_CH26		EQU %00011010	;ADCS1 configured for CH26 (internal temp. sensor)
             
 
 ; export symbols
-            XDEF _Startup, main, _Vtpmovf, bus_write, bus_read, keypad_scan, led_write, lcd_init, SUB_delay
+            XDEF _Startup, main, _Vtpmovf, keypad_scan, led_write, lcd_init, SUB_delay
             ; we export both '_Startup' and 'main' as symbols. Either can
             ; be referenced in the linker .prm file or from C/C++ later on
             
             
             XREF __SEG_END_SSTACK   ; symbol defined by the linker for the end of the stack
+            XREF bus_read, bus_write
+            ;XREF LCD_ASM
+            ;XREF ADC_ASM
 
 
 ; variable/data section
@@ -106,7 +110,7 @@ _Startup:
 			
 
 mainLoop:
-;*** rest to start
+;*** reset to start
 			; clear vars 
 			
 			
@@ -155,6 +159,10 @@ mainloop_read:
 
 			
 ;*** do math internal temp data
+
+
+;*** displau result
+mainloop_end:
            	
             feed_watchdog
             BRA    	mainLoop
@@ -322,85 +330,6 @@ led_write:
 			PULA			
 			RTS
 
-
-;**************************************************************
-
-;************************************************************** 
-;* Subroutine Name: bus_read 
-;* Description: Reads data from the device whose address is
-;*				the lower 3 bits of bus_addr, and store the
-;*				data to the lower 4 bits of bus_data.
-;* 
-;* Registers Modified: None
-;* Entry Variables: bus_addr
-;* Exit Variables: bus_data
-;**************************************************************
-bus_read:
-			; preserve accumulator A
-			PSHA
-
-			; make address bus output, data bus an input
-            LDA		#mAddrBus
-            STA		PTBDD
-            
-			; pull the address low
-            LDA 	bus_addr			; load address
-            AND		#$07				; mask off the lower 3 bits to be sure, will leave G2A low
-            STA		PTBD				; write data to address bus, and clear data bus
-            
-            ; read data from the bus
-            LDA		PTBD
-            NSA 						; shift data down to the lower 4 bits
-            AND		#$0F				; mask off the lower 4 bits to be sure
-            STA		bus_data			; 
-            
-			; pull the address high
-            LDA 	#$08				; G2A_not high
-            STA		PTBD				; write, clears address bus
-            
-			; restore accumulator A
-			PULA
-			
-			; return from subroutine bus_read
-			RTS
-
-;**************************************************************
-
-
-;************************************************************** 
-;* Subroutine Name: bus_write 
-;* Description: Writes the lower 4 bits of bus_data to the 
-;*				device on whose address is the lower 3 bits
-;* 				of bus_addr. 
-;* Registers Modified: None
-;* Entry Variables: bus_addr, bus_data
-;* Exit Variables: None 
-;**************************************************************
-bus_write:
-			; preserve accumulator A
-			PSHA			
-			
-			; make data and address busses outputs
-            LDA		#$FF
-            STA		PTBDD
-            
-            ; prep data for the bus
-            LDA		bus_data
-            NSA 						; swap the lower 4 bits to be the upper 4 bits
-            AND		#$F0				; mask off the upper 4 bits to be sure
-            
-			; prep the addr, G2A_not low, Yx goes low
-            ORA		bus_addr 			; add in the address
-            STA		PTBD				; write data and address bus, with G2A_not low            
-            
-            ORA		#$08				; leave data and address, set G2A_not high - Yx goes high
-            STA		PTBD
-            
-			; restore accumulator A
-			PULA
-			
-			; return from subroutine bus_write
-			RTS
 
 ;**************************************************************
 
